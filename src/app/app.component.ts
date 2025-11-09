8e67309c0a0dcd3cbd13230b73465f3e6cd0583d
Ameyalli <ameyanezcmm@gmail.com>
2025-11-08 19:12:52 -0600

    primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, 
+    HeroComponent, AboutComponent, PortfolioComponent,
+    ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
