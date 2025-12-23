5a08605710e6ec625ed5befacb23d62849a3c11d
Ameyalli <ameyanezcmm@gmail.com>
2025-12-22 20:52:07 -0600

    feat: adding about me text

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  title:string = 'Acerca de mí'; 
+
+  texto_1:string =` Soy una desarrolladora frontend con un fuerte interés en el diseño UI/UX, apasionada por crear experiencias digitales claras, funcionales y centradas en las personas. Encuentro en la programación web un espacio donde la lógica, la estética y la creatividad convergen.`;
+  texto_2:string =`Además de la tecnología, la literatura es una parte fundamental de mi vida; en especial la ciencia ficción, género que inspira mi curiosidad por los futuros posibles y las narrativas no convencionales. Los miserables de Victor Hugo es uno de mis libros favoritos, por su profundidad humana y su mirada crítica sobre la sociedad.`;
+  texto_3:string =`En cada proyecto pongo un alto nivel de dedicación, buscando siempre la calidad, la eficacia y la mejora continua. Aprender, actualizarme y seguir creciendo tanto a nivel técnico como creativo es un valor central en mi desarrollo profesional.`;
 }
